ba1cf6d6dc6add712f1b0285f1e00619a070ee1c
Sebastian Georgi <sebastian@georgi.io>
2020-12-09 15:25:48 +0100

    Changed from minutes to hours

--- test/helpers/time_helper_test.rb
+++ test/helpers/time_helper_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class TimeHelperTest < ActiveSupport::TestCase
   include TimeHelper
 
-  test '#minutes_since_last_tweet' do
-    assert minutes_since_last_tweet > 0
+  test '#hoursa_since_last_tweet' do
+    assert hours_since_last_tweet > 0
   end
 end
